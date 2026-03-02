6f12ef3f181d9863b26dd7ff0c269a4826fd90ae
Professor Aécio <professoraecio@gmail.com>
2026-03-02 19:21:33 -0300

    Add Projeto ScrollView

---
+++ Exemplo Scroll Horizontal/Assets/Scripts/Nuvens.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Nuvens : MonoBehaviour
+{
+    public float velocidadeAnimacao = 0.1f;
+    public Renderer textura;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime = tempo em segundos independente do fps
+        // Definir o deslocamento da imagem baseado na velocidade
+        Vector2 deslocamentoImagem = 
+        new Vector2(velocidadeAnimacao * Time.deltaTime, 0);
+        // Aplicar a textura baseado no acrescimo do
+        // deslocamento da imagem de forma incremental
+        textura.material.mainTextureOffset =
+        textura.material.mainTextureOffset + deslocamentoImagem;
+    }
+}
