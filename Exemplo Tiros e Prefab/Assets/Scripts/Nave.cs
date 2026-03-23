aa252508b07fd781eb5d5c3faa34f9b1073d8a07
Professor Aécio <professoraecio@gmail.com>
2026-03-23 20:26:12 -0300

    Projeto parcial...

---
+++ Exemplo Tiros e Prefab/Assets/Scripts/Nave.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    [Header("Tiro")]
+    [SerializeField] public GameObject missil;
+    [SerializeField] public GameObject origemMissil;
+    [SerializeField] public float intervaloTiro = 1f; // diferença de 1 segundo por tiro
+    [SerializeField] public float tempoProximoTiro = 0f;
+    [Header("Movimento")]
+    [SerializeField] public float velocidade = 6f;
+    [SerializeField] private UtilScreenBoundsLimiter bounds;
+    UtilKeyboardInput utilKeyboardInput;
+    void Start()
+    {
+        if(bounds == null) bounds = GetComponent<UtilScreenBoundsLimiter>();
+        utilKeyboardInput = GetComponent<UtilKeyboardInput>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ProcessarMovimento()
+    {
+        float dirX = 0f;
+        float dirY = 0f;
+        if(utilKeyboardInput.leftPressed) dirX -= 1f;
+        if(utilKeyboardInput.rightPressed) dirX += 1f;
+        if(utilKeyboardInput.upPressed) dirY += 1f;
+        if(utilKeyboardInput.downPressed) dirY -= 1f;
+        Vector3 pos = transform.position;
+        pos.x += dirX * velocidade * Time.deltaTime;
+        pos.y += dirY * velocidade * Time.deltaTime;
+        transform.position = pos;
+        if(bounds != null) transform.position = bounds.GetCoordenadasLimite(pos);
+    }
+
+}
---
+++ Exemplo Tiros e Prefab/Assets/Scripts/UtilKeyboardInput.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class UtilKeyboardInput : MonoBehaviour
+{
+    public bool upPressed = false;
+    public bool downPressed = false;
+    public bool leftPressed = false;
+    public bool rightPressed = false;
+    public bool spacePressed = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        upPressed = false;
+        downPressed = false;
+        leftPressed = false;
+        rightPressed = false;
+        spacePressed = false;
+        if (Keyboard.current.upArrowKey.isPressed)
+            upPressed = true;
+        if (Keyboard.current.downArrowKey.isPressed)
+            downPressed = true;
+        if (Keyboard.current.leftArrowKey.isPressed)
+            leftPressed = true;
+        if (Keyboard.current.rightArrowKey.isPressed)
+            rightPressed = true;
+        if (Keyboard.current.spaceKey.isPressed)
+            spacePressed = true;
+    }
+}
