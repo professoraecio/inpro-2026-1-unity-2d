5a9f0dbbdd5edc3fcdf55239787713356ba2aa12
Aecio Marques Teixeira <professoraecio@gmail.com>
2026-03-29 21:41:39 -0300

    Atualizar projeto

---
+++ 07 Trigger/Assets/Scripts/UtilKeyboardInput.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class UtilKeyboardInput : MonoBehaviour
+{
+    public bool cima = false;
+    public bool baixo = false;
+    public bool esquerda = false;
+    public bool direita = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cima = false;
+        baixo = false;
+        esquerda = false;
+        direita = false;
+        if (Keyboard.current.upArrowKey.isPressed)
+            cima = true;
+        if (Keyboard.current.downArrowKey.isPressed)
+            baixo = true;
+        if (Keyboard.current.leftArrowKey.isPressed)
+            esquerda = true;
+        if (Keyboard.current.rightArrowKey.isPressed)
+            direita = true;
+    }
+}
