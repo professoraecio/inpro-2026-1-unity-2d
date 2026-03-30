724124a18dc6fde80d8776a0d54d5dbb9388a071
Aecio Marques Teixeira <professoraecio@gmail.com>
2026-03-29 22:25:08 -0300

    Enhance code

--- 07 Trigger/Assets/Scripts/BigRing.cs
+++ 07 Trigger/Assets/Scripts/BigRing.cs
@@ -12,18 +12,19 @@ public class BigRing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = Time.deltaTime;
+        
         if(animaisFuriosos)
         {
-            sonic.gameObject.transform.Rotate(Roll(1000 * speed));
-            tails.gameObject.transform.Rotate(Roll(50 * speed));
-            knuckles.gameObject.transform.Rotate(Roll(400 * speed));
+            sonic.gameObject.transform.Rotate(Roll(1000));
+            tails.gameObject.transform.Rotate(Roll(50));
+            knuckles.gameObject.transform.Rotate(Roll(400));
         }
     }
 
     private Vector3 Roll(float r)
     {
-        return new Vector3(0, 0, r);
+        float speed = Time.deltaTime;
+        return new Vector3(0, 0, r * speed);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
