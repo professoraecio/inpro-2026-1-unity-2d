2ded959287a71238f244110fe1b6909e757d7c2d
Professor Aécio <professoraecio@gmail.com>
2026-03-30 19:38:35 -0300

    Mover eggman

---
+++ Exemplo Trigger/Assets/Scripts/EggMan.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EggMan : MonoBehaviour
+{
+    UtilKeyboardInput teclado;
+    [SerializeField]
+    float velocidade = 2.5f;
+    void Start()
+    {
+        teclado = GetComponent<UtilKeyboardInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = velocidade * Time.deltaTime;
+        if(teclado.cima)
+            transform.Translate(Mover(0,deslocamento));
+        if(teclado.baixo)
+            transform.Translate(Mover(0,-deslocamento));
+        if(teclado.esquerda)
+            transform.Translate(Mover(-deslocamento,0));
+        if(teclado.direita)
+            transform.Translate(Mover(deslocamento,0));
+    }
+
+    private Vector3 Mover(float x,float y)
+    {
+        return new Vector3(x,y,0);
+    }
+}
