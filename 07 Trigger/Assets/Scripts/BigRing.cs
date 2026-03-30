214c8bbf95a7163ea0fbffdcff0dada323696b32
Aecio Marques Teixeira <professoraecio@gmail.com>
2026-03-29 22:23:34 -0300

    update

--- 07 Trigger/Assets/Scripts/BigRing.cs
+++ 07 Trigger/Assets/Scripts/BigRing.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class BigRing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool animaisFuriosos = false;
+    public GameObject sonic,tails,knuckles;
     void Start()
     {
         
@@ -11,6 +12,28 @@ public class BigRing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Time.deltaTime;
+        if(animaisFuriosos)
+        {
+            sonic.gameObject.transform.Rotate(Roll(1000 * speed));
+            tails.gameObject.transform.Rotate(Roll(50 * speed));
+            knuckles.gameObject.transform.Rotate(Roll(400 * speed));
+        }
+    }
+
+    private Vector3 Roll(float r)
+    {
+        return new Vector3(0, 0, r);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("EggMan"))
+            animaisFuriosos = true;
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("EggMan"))
+            animaisFuriosos = false;
     }
 }
