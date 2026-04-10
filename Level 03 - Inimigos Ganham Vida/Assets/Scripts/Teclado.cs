85824faf3efcebc358d7befc041f38eb7580716a
Professor Aécio <professoraecio@gmail.com>
2026-04-09 21:28:02 -0300

    Add Level 03

---
+++ Level 03 - Inimigos Ganham Vida/Assets/Scripts/Teclado.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Teclado : MonoBehaviour
+{
+    public bool cima;
+    public bool baixo;
+    public bool esquerda;
+    public bool direita;
+    public bool z;
+    public bool x;
+    
+    void Update()
+    {
+        if(Keyboard.current == null) return;
+        cima = Keyboard.current.upArrowKey.isPressed;
+        baixo = Keyboard.current.downArrowKey.isPressed;
+        esquerda = Keyboard.current.leftArrowKey.isPressed;
+        direita = Keyboard.current.rightArrowKey.isPressed;
+        z = Keyboard.current.zKey.isPressed;
+        x = Keyboard.current.xKey.isPressed;
+    }
+}
