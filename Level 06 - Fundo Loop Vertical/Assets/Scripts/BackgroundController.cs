eb2ed97d84846b9337668421dc3eb1a9ba1cca5f
Professor Aécio <professoraecio@gmail.com>
2026-04-16 09:59:28 -0300

    update projeto back loop

--- Level 06 - Fundo Loop Vertical/Assets/Scripts/BackgroundController.cs
+++ Level 06 - Fundo Loop Vertical/Assets/Scripts/BackgroundController.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
-
 public class BackgroundController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool vertical = true;
+    public bool horizontal = false;
+    [SerializeField]
+    public float velocidade = 0.8f;
+    public Renderer render;
+    Vector2 offset;
+    float deslocamento;
     void Start()
     {
-        
+        render = GetComponent<Renderer>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        deslocamento = velocidade * Time.deltaTime;
+        if(vertical)
+            offset = new Vector2(0,deslocamento);
+        if(horizontal)
+            offset = new Vector2(deslocamento,0);
+        render.material.mainTextureOffset += offset;
     }
 }
